4ea63816c56bdb8d9b7020a8166687cb18da9c2e
me-coderaider <kashyappankajnsp@gmail.com>
2023-08-22 18:05:11 +0530

    adding all files 1

---
+++ src/counter/Counter.jsx
@@ -0,0 +1,12 @@
+import React, {Compnent} from 'react';
+class Counter extends Compnent{
+    render(){
+        return (
+            <div>
+                this is our counter
+            </div>
+        )
+    }
+}
+
+export default Counter; 
