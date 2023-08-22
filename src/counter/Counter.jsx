fcaae3d13b786f5a2037f3262a2d10401abef7ce
me-coderaider <kashyappankajnsp@gmail.com>
2023-08-22 18:33:49 +0530

    added counting functionality 3

--- src/counter/Counter.jsx
+++ src/counter/Counter.jsx
@@ -7,13 +7,22 @@ class Counter extends Component{
         this.state={
             counter : 0
         }
+        this.clickCounter=this.clickCounter.bind(this);
+    }
+
+    clickCounter(){
+        // console.log(this.state.counter)
+        
+        this.setState({
+            counter : this.state.counter + 1
+        })
     }
 
     render(){
         return (
             <div>
-                <button className='button'>click here</button>
-                <span className='count'>0</span>
+                <button className='button' onClick={this.clickCounter}>Click Here</button>
+                <span className='count'>{this.state.counter}</span>
             </div>
         )
     }
