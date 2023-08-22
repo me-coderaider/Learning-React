315cf04d9ca9cd292e25317fb9183e06d2017c0a
me-coderaider <kashyappankajnsp@gmail.com>
2023-08-22 18:21:24 +0530

    added more files 2

--- src/counter/Counter.jsx
+++ src/counter/Counter.jsx
@@ -1,9 +1,19 @@
-import React, {Compnent} from 'react';
-class Counter extends Compnent{
+import React, {Component} from 'react';
+import './Counter.css'
+class Counter extends Component{
+    constructor(props){
+        super(props);
+
+        this.state={
+            counter : 0
+        }
+    }
+
     render(){
         return (
             <div>
-                this is our counter
+                <button className='button'>click here</button>
+                <span className='count'>0</span>
             </div>
         )
     }
